d1a23af9173f7720caac9997784e55054eb70971
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-07 00:02:54 +0800

    remove image and styles

--- src/styles/images.js
+++
@@ -1,6 +0,0 @@
-export const images = {
-    arrow: require('../assets/images/arrow.png'),
-    loading: require('../assets/images/loading.gif'),
-    symbol: require('../assets/images/symbol.png'),
-
-}
