ac56aacc518474845882605148d01c760f7ee9df
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-08 09:46:19 +0800

    add backendless

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import NavigationContainer from './src/navigation/NavigationContainer';
+import Backendless from 'backendless';
+
+const APP_ID = 'DAC4D020-9129-B7B3-FFBC-E433549A3400';
+const APP_KEY = '310C66D2-D021-4997-940D-4E946F04C9FD';
+Backendless.initApp(APP_ID, APP_KEY);
 
 export default class App extends Component {
   render() {
-    return (
-        <NavigationContainer />
-    );
+    return <NavigationContainer />;
   }
 }
